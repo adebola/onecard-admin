e06ce501ca2d40f434c6ab933e24fa223a1817ae
Adebola Omoboya <adeomoboya@gmail.com>
2023-07-18 19:54:27 +0100

    Implement Provider Wallet Report

--- src/app/reports/wallet/provider/provider-wallet.component.ts
+++ src/app/reports/wallet/provider/provider-wallet.component.ts
@@ -1,37 +1,70 @@
 import {Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import {UserDatasource} from '../../../shared/datasource/user.datasource';
-import {Observable} from 'rxjs';
-import {RechargeProvider} from '../../../shared/model/recharge-provider.model';
-import {ProviderService} from '../../../shared/service/provider.service';
-import {Page} from '../../../shared/service/utility/page';
-import {map} from 'rxjs/operators';
+import {finalize} from 'rxjs/operators';
+import {Subscription} from 'rxjs/Subscription';
+import {ReportService} from '../../../shared/service/report.service';
+import {UtilityService} from '../../../shared/service/utility.service';
 
 @Component({
     selector: 'app-provider-wallet',
     templateUrl: './provider-wallet.component.html'
 })
 export class ProviderWalletComponent implements OnInit, OnDestroy {
-
-    @ViewChild('input') input: ElementRef;
-
     public busy: boolean;
-    public providerForm: FormGroup;
-    public selectedRowIndex: number;
-    public datasource: UserDatasource;
-    public services$: Observable<RechargeProvider[]>;
+    public providerWalletForm: FormGroup;
+    private subscription: Subscription = null;
+    public reportType = ['short', 'long'];
 
     constructor(@Inject(FormBuilder) private fb: FormBuilder,
-                private providerService: ProviderService) {}
+                private reportService: ReportService,
+                private utilityService: UtilityService) {}
 
     ngOnDestroy(): void {
+        if (this.subscription) { this.subscription.unsubscribe(); }
     }
 
     ngOnInit(): void {
-        this.services$ = this.providerService.getAllRechargeProviders(1, 50)
-            .pipe(map(r => r.list));
+        this.createForm();
     }
 
-    onSubmit(providerForm: FormGroup) {
+    onSubmit(providerWalletForm: FormGroup) {
+        const type: string = providerWalletForm.value.type;
+        const start: Date = providerWalletForm.value.start;
+        const end: Date = providerWalletForm.value.end;
+
+        this.busy = true;
+
+        let startDate: string = null;
+        let endDate: string = null;
+
+        if (start) {
+            startDate = this.utilityService.stringifyDateForJson(start);
+        }
+
+        if (end) {
+            endDate = this.utilityService.stringifyDateForJson(end);
+        }
+
+        this.subscription = this.reportService.runWalletReport({
+            type: type,
+            startDate: startDate,
+            endDate: endDate
+        }).pipe(
+            finalize(() => this.busy = false)
+        ).subscribe(data => {
+            const blob =
+                new Blob([data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+            const fileURL = URL.createObjectURL(blob);
+            this.busy = false;
+            window.open(fileURL);
+        });
+    }
+
+    private createForm() {
+        this.providerWalletForm = this.fb.group({
+            type: ['short'],
+            start: [null],
+            end: [null]
+        });
     }
 }
